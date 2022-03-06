a6debb85b144c27d9d8102439be24c55d1726680
med jawhar nabi <mohamedjawhar.nabi@redlean.io>
2022-03-06 14:47:14 +0100

    add loader at enter

--- src/App.js
+++ src/App.js
@@ -5,18 +5,46 @@ import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Testimonials from "./components/Testimonials";
-import ScrollToTopBtn from "./components/Back-to-top"
-export default function App() {
-  return (
-    <main text-gray-400 bg-gray-900 body-font>
-      <Navbar />
-      <About />
-      <Projects />
-      <Skills />
-      <Testimonials />
-      <Contact />
-      <ScrollToTopBtn />
+import ScrollToTopBtn from "./components/Back-to-top";
+import ReactDOM from 'react-dom';
 
-    </main>
-  );
+export default class App extends React.Component {
+  state = {
+    loading: true
+  };
+
+  componentDidMount() {
+    // this simulates an async action, after which the component will render the content
+    demoAsyncCall().then(() => this.setState({ loading: false }));
+  }
+
+  render() {
+    const { loading } = this.state;
+
+    if (loading) { // if your component doesn't have to wait for an async action, remove this block 
+      return null; // render null when app is not ready
+    }
+    return (
+      <main text-gray-400 bg-gray-900 body-font>
+        <Navbar />
+        <About />
+        <Projects />
+        <Skills />
+        <Testimonials />
+        <Contact />
+        <ScrollToTopBtn />
+
+      </main>
+    );
+  }
 }
+
+function demoAsyncCall() {
+  return new Promise((resolve) => setTimeout(() => resolve(), 2500));
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
